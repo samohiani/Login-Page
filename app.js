2b09f6b3cc4e3405f4aa0f8047f61f898613c8e9
samohiani <ohianisammy2005@gmail.com>
2025-05-09 15:00:42 +0100

    changes

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const session = require("express-session");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
-const MongoStore = require("connect-mongo");
 
 dotenv.config();
 const app = express();
@@ -11,20 +10,6 @@ connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(
-  session({
-    secret: "your-session-secret",
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({
-      mongoUrl: process.env.MONGODB_URI,
-      collectionName: "sessions",
-    }),
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24, // 1 day
-    },
-  })
-);
 
 app.set("view engine", "ejs");
 
